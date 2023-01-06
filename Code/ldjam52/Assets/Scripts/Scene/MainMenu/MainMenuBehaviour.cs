2babc5daa80ce67f58a968f274650e51cb45b220
Rentem <salasam@gmx.net>
2023-01-07 00:39:42 +0100

    stuff

--- Code/ldjam52/Assets/Scripts/Scene/MainMenu/MainMenuBehaviour.cs
+++ Code/ldjam52/Assets/Scripts/Scene/MainMenu/MainMenuBehaviour.cs
@@ -1,46 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+
+using Assets.Scripts.Base;
+using Assets.Scripts.Constants;
+
+using GameFrame.Core.Audio.Multi;
+using GameFrame.Core.Audio.Single;
+
 using UnityEngine;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
     public void ShowSavedGames()
     {
-        Core.Game.PlayButtonSound();
+        //Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.SavedGames);
     }
 
     public void ShowModes()
     {
-        Core.Game.PlayButtonSound();
-        Core.Game.ChangeScene(SceneNames.GameModeScene);
+        //Core.Game.PlayButtonSound();
+        Core.Game.ChangeScene(SceneNames.GameMode);
     }
 
     public void ShowOptions()
     {
-        Core.Game.PlayButtonSound();
-        Core.Game.ChangeScene(SceneNames.OptionsScene);
+        //Core.Game.PlayButtonSound();
+        Core.Game.ChangeScene(SceneNames.Options);
     }
 
     public void ShowCredits()
     {
-        Core.Game.PlayButtonSound();
+        //Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.Credits);
     }
 
     public void StartGame()
     {
-        if (Assets.Scripts.Base.Core.Game.Options.ShowTutorial)
-        {
-            Core.Game.PlayButtonSound();
-            Core.Game.ChangeScene(SceneNames.Tutorial);
-        }
-        else
-        {
-            Core.Game.PlayButtonSound();
-            Core.Game.Start();
+        //if (Assets.Scripts.Base.Core.Game.Options.ShowTutorial)
+        //{
+        //    Core.Game.PlayButtonSound();
+        //    Core.Game.ChangeScene(SceneNames.Tutorial);
+        //}
+        //else
+        //{
+        //    Core.Game.PlayButtonSound();
+        //    Core.Game.Start();
 
-        }
+        //}
     }
 
 }
