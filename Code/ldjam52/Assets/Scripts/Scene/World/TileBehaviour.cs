70f41a0248a3a96ff89488ca2c559d9d1926ab28
XpArKeR <xparker@gmx.net>
2023-01-07 20:39:04 +0100

    Started rendering

---
+++ Code/ldjam52/Assets/Scripts/Scene/World/TileBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts.Model;
+
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+    public Tile Tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Code/ldjam52/Assets/Scripts/Scene/World/WorldBehaviour.cs
@@ -0,0 +1,46 @@
+using Assets.Scripts.Model;
+
+using UnityEngine;
+
+public class WorldBehaviour : MonoBehaviour
+{
+    private GameObject tileContainer;
+    private GameObject templateContainer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        this.tileContainer = transform.Find("TileContainer").gameObject;
+        this.templateContainer = transform.Find("TemplateContainer").gameObject;
+
+        var gameState = Assets.Scripts.Base.Core.Game.State;
+
+        if (gameState != default)
+        {
+            if (gameState.World.Tiles.Count > 0)
+            {
+                RenderWorld(gameState.World);
+            }
+        }
+    }
+
+    private void RenderWorld(World world)
+    {
+        var tileTemplate = default(GameObject);
+
+        foreach (var tile in world.Tiles)
+        {
+            var tileGameObject = Instantiate(tileTemplate, this.tileContainer.transform);
+
+            var tileBehaviour = tileGameObject.GetComponent<TileBehaviour>();
+
+            tileBehaviour.Tile = tile;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
