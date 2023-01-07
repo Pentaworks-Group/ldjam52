410afd40cb2c51e7524db75c3c7d89bd031a8927
Lea Scherer <38538244+leas251@users.noreply.github.com>
2023-01-07 10:11:56 +0100

    Update Chromosome Model
    Added Chromosome Model,
    Added Inheritance Controller,
    Adjusted Plant Model

---
+++ Code/ldjam52/Assets/Scripts/Model/Chromosome.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Model
+{
+    public class Chromosome
+    {
+        public Double Value_0 { get; set; }
+        public Double Value_dev { get; set; }
+        public Boolean dominant { get; set; }
+    }
+}
--- Code/ldjam52/Assets/Scripts/Model/Plant.cs
+++ Code/ldjam52/Assets/Scripts/Model/Plant.cs
@@ -9,29 +9,31 @@ namespace Assets.Scripts.Model
     public class Plant
     {
         public String Name { get; set; }
-        public Double Temp_0 { get; set; }
-        public Double Temp_dev { get; set; }
-        public Double Sun_0 { get; set; }
-        public Double Sun_dev { get; set; }
-        public Double Fertilty_0 { get; set; }
-        public Double Fertility_dev { get; set; }
-        public Double Water_0 { get; set; }
-        public Double Water_dev { get; set; }
-        public Double Growth_min { get; set; }
-        public Double Growth_max { get; set; }
-        public Double Age_min { get; set; }
-        public Double Age_max { get; set; }
-        public Double Wither_min { get; set; }
-        public Double Wither_max { get; set; }
-        public Double Durability_min { get; set; }
-        public Double Durability_max { get; set; }
-        public Double Harvest_min { get; set; }
-        public Double Harvest_max { get; set; }
-        public Double Seeds_min { get; set; }
-        public Double Seeds_max { get; set; }  
-        public Double Plant_Value_min { get; set; }
-        public Double Plant_Value_max { get; set; }
-        public Double Seeds_Value_min { set; get; }
-        public Double Seeds_Value_max { set; get; }
+        public String Description { get; set; }
+        public String ImageName { get; set; }
+        public Chromosome Temp_1 { get; set; }
+        public Chromosome Temp_2 { get; set; }
+        public Chromosome Sun_1 { get; set; }
+        public Chromosome Sun_2 { get; set; }
+        public Chromosome Fertility_1 { get; set; }
+        public Chromosome Fertility_2 { get; set; }
+        public Chromosome Water_1 { get; set; }
+        public Chromosome Water_2 { get; set; }
+        public Chromosome Growth_1 { get; set; }
+        public Chromosome Growth_2 { get; set; }
+        public Chromosome Age_1 { get; set; }
+        public Chromosome Age_2 { get; set; }
+        public Chromosome Wither_1 { get; set; }
+        public Chromosome Wither_2 { get; set; }
+        public Chromosome Durabilty_1 { get; set; }
+        public Chromosome Durabilty_2 { get; set; }
+        public Chromosome Harvest_1 { get; set; }
+        public Chromosome Harvest_2 { get; set; }
+        public Chromosome Seeds_1 { get; set; }
+        public Chromosome Seeds_2 { get; set; }
+        public Chromosome Plant_Value_1 { get; set; }
+        public Chromosome Plant_Value_2 { get; set; }
+        public Chromosome Seeds_Value_1 { get; set; }
+        public Chromosome Seeds_Value_2 { get; set; }
     }
 }
