e8be6e09de76aa14b0b09928b060b42d3bea6d62
Lea Scherer <38538244+leas251@users.noreply.github.com>
2023-01-07 13:16:08 +0100

    GrowthController
    Added GrowthController

---
+++ Code/ldjam52/Assets/Scripts/Core/GrowthController.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts.Model;
+using Assets.Scripts.Constants;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MathNet.Numerics.Distributions;
+using UnityEngine;
+using GameFrame.Core.Extensions;
+
+public class GrowthController : MonoBehaviour
+{
+}
