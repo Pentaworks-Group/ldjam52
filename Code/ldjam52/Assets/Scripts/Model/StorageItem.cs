9c17f6b49f2b7055a84ce159433f224828d180bc
Rentem <salasam@gmx.net>
2023-01-08 00:04:38 +0100

    added storage to gamemode init for TestFieldBehaviour

--- Code/ldjam52/Assets/Scripts/Model/StorageItem.cs
+++ Code/ldjam52/Assets/Scripts/Model/StorageItem.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 namespace Assets.Scripts.Model
 {
     public class StorageItem
     {
-        public Plant Plant { get; set; }
+        public Guid? PlantId
+        {
+            get;
+            set;
+        }
+
+
+        private Plant plant;
+
+        [JsonIgnore]
+        public Plant Plant
+        {
+            get
+            {
+                if (plant == default)
+                {
+                    if (PlantId.HasValue && Assets.Scripts.Base.Core.Game.State.KnownPlants.TryGetValue(PlantId.Value, out Plant planti))
+                    {
+                        plant = planti;
+                    }
+                }
+                return plant;
+            }
+            set
+            {
+                this.plant = value;
+                this.PlantId = value?.ID;
+            }
+        }
         public int StorageAmountPlants { get; set; }
         public int StorageAmountSeeds { get; set; }
     }
