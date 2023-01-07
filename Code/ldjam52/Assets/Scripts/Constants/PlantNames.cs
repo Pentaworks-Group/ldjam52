8ff4189ec30e0b2e3247ead2a76a61176cef64a9
Lea Scherer <38538244+leas251@users.noreply.github.com>
2023-01-07 12:45:46 +0100

    Added Naming Lists

---
+++ Code/ldjam52/Assets/Scripts/Constants/PlantNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Constants
+{
+    internal class NameConstants
+    {
+        public static List<String> adjectives = new List<String>
+        {
+            "normal"
+        };
+
+        public static List<String> names = new List<String>
+        {
+            "normal"
+        };
+
+    }
+}
