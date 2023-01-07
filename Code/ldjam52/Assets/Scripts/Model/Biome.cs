993f743efb69c13e1a627e0004076ef5ee27cd7c
Lea Scherer <38538244+leas251@users.noreply.github.com>
2023-01-07 10:50:56 +0100

    Models Update
    Updated Biome and Field

--- Code/ldjam52/Assets/Scripts/Model/Biome.cs
+++ Code/ldjam52/Assets/Scripts/Model/Biome.cs
@@ -5,12 +5,16 @@ namespace Assets.Scripts.Model
     public class Biome
     {
         public String Name { get; set; }
+        //Game Value: Possible Humidity values for the Biome
         public Double Humidity_min { get; set; }
         public Double Humidity_max { get; set; }
+        //Game Value: Possible Temperature values for the Biome
         public Double Temperature_min { get; set; }
         public Double Temperature_max { get; set; }
+        //Game Value: Possible Sunhsine values for the Biome
         public Double Sunshine_min { get; set; }
         public Double Sunshine_max { get; set; }
+        //Game Value: Possible Fertility values for the Biome
         public Double Fertility_min { get; set; }
         public Double Fertility_max { get; set; }
     }
--- Code/ldjam52/Assets/Scripts/Model/Field.cs
+++ Code/ldjam52/Assets/Scripts/Model/Field.cs
@@ -9,5 +9,15 @@ namespace Assets.Scripts.Model
     public class Field
     {
         public Plant Seed { get; set; }
+        // Humidity of the field
+        public Double Humidity { get; set; }
+        // Temperature of the Biome
+        public Double Temperature { get; set; }
+        // Sunsihne of the field
+        public Double Sunshine { get; set; }
+        // Fertility of the field
+        public Double Fertility { get; set; }
+
+
     }
 }
