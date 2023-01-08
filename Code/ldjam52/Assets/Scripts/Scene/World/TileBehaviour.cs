42eccf02de689670ffeadb106e861e98b4cf7a13
XpArKeR <xparker@gmx.net>
2023-01-08 10:01:54 +0100

    Started tile refactoring

--- Code/ldjam52/Assets/Scripts/Scene/World/TileBehaviour.cs
+++ Code/ldjam52/Assets/Scripts/Scene/World/TileBehaviour.cs
@@ -12,18 +12,17 @@ public class TileBehaviour : MonoBehaviour
 
     private GameObject floorGameObject;
     private Boolean isColorSet;
+    private Boolean isTileOwned;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         this.floorGameObject = transform.Find("Fliese").gameObject;
-    }
+        
 
-    // Update is called once per frame
-    void Update()
-    {
         if (this.Tile != default)
         {
+            this.isTileOwned = this.Tile.IsOwned;
+
             if (!isColorSet && this.floorGameObject != null)
             {
                 if (this.floorGameObject.TryGetComponent<MeshRenderer>(out var meshRenderer))
@@ -36,5 +35,37 @@ public class TileBehaviour : MonoBehaviour
                 }
             }
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.Tile != default)
+        {
+            if (this.Tile.IsOwned && !isTileOwned)
+            {
+                transform.Find("Field").gameObject.SetActive(true);
+                transform.Find("NatureArea").gameObject.SetActive(false);
+            }
+        }
+        //if (this.Tile != default)
+        //{
+        //    if (!isColorSet && this.floorGameObject != null)
+        //    {
+        //        if (this.floorGameObject.TryGetComponent<MeshRenderer>(out var meshRenderer))
+        //        {
+        //            if (meshRenderer.material != null)
+        //            {
+        //                meshRenderer.material.color = this.Tile.Color.ToUnity();
+        //                this.isColorSet = true;
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
