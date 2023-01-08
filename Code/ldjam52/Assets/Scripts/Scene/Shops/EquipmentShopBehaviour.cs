b2db5effd72e6facc87efff666a5454af9869307
Lea Scherer <38538244+leas251@users.noreply.github.com>
2023-01-08 20:12:57 +0100

    Create EquipmentShopBehaviour.cs

---
+++ Code/ldjam52/Assets/Scripts/Scene/Shops/EquipmentShopBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Scene.Shops
+{
+    public class EquipmentShopBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+    }
+
+}
