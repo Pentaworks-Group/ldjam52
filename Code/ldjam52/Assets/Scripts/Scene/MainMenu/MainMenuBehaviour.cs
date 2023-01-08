acc43219cb5f921ff41d420443ea57ff3ef5789b
XpArKeR <xparker@gmx.net>
2023-01-08 18:39:11 +0100

    Changes in GameFrame.

--- Code/ldjam52/Assets/Scripts/Scene/MainMenu/MainMenuBehaviour.cs
+++ Code/ldjam52/Assets/Scripts/Scene/MainMenu/MainMenuBehaviour.cs
@@ -17,25 +17,25 @@ public class MainMenuBehaviour : MonoBehaviour
 
     public void ShowSavedGames()
     {
-        //Core.Game.PlayButtonSound();
+        Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.SavedGames);
     }
 
     public void ShowModes()
     {
-        //Core.Game.PlayButtonSound();
+        Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.GameMode);
     }
 
     public void ShowOptions()
     {
-        //Core.Game.PlayButtonSound();
+        Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.Options);
     }
 
     public void ShowCredits()
     {
-        //Core.Game.PlayButtonSound();
+        Core.Game.PlayButtonSound();
         Core.Game.ChangeScene(SceneNames.Credits);
     }
 
