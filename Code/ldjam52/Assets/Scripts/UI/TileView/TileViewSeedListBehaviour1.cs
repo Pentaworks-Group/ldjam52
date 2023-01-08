9101985e34b81465f119eb4cb9c5a216ae743212
Rentem <salasam@gmx.net>
2023-01-08 13:06:39 +0100

    test listbehaviour refined tileViewBehaviour

---
+++ Code/ldjam52/Assets/Scripts/UI/TileView/TileViewSeedListBehaviour1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts.Core.Inventory;
+using Assets.Scripts.Model;
+
+using UnityEngine;
+
+public class TileViewSeedListBehaviour1 : ListCont
+{
+    override public void CustomStart()
+    {
+
+        UpdateList();
+    }
+
+    public void UpdateList()
+    {
+        List<System.Object> plants = new();
+        foreach (StorageItem item in FarmStorageController.getStorageInventory())
+        {
+            if (item.StorageAmountSeeds > 0)
+            {
+                plants.Add(item);
+            }
+        }
+        Debug.Log(plants.Count);
+
+        SetObjects(plants);
+    }
+
+}
