c3633220e9ed642226c2e04fbf38a985b4bf5169
Rentem <salasam@gmx.net>
2023-01-09 14:23:45 +0100

    Added esc for back in some scenes

--- Code/ldjam52/Assets/Scripts/Scene/Options/OptionsSceneBehaviour.cs
+++ Code/ldjam52/Assets/Scripts/Scene/Options/OptionsSceneBehaviour.cs
@@ -1,6 +1,14 @@
 using Assets.Scripts.Scenes;
 
+using UnityEngine;
+
 public class OptionsSceneBehaviour : BaseMenuBehaviour
 {
- 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToMainMenu();
+        }
+    }
 }
--- Code/ldjam52/Assets/Scripts/Scene/SaveGame/SaveGameSceneBehaviour.cs
+++ Code/ldjam52/Assets/Scripts/Scene/SaveGame/SaveGameSceneBehaviour.cs
@@ -1,8 +1,17 @@
 using Assets.Scripts.Scenes;
+
+using UnityEngine;
+
 namespace Assets.Scripts.Scene.SaveGame
 {
     public class SaveGameSceneBehaviour : BaseMenuBehaviour
     {
-
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ToMainMenu();
+            }
+        }
     }
 }
